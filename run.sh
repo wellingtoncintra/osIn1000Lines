a13b3dd8ba6d6ca786c3b6b2b2fcaf14bbe4d08a
Wellington Cintra <well.cintra00@gmail.com>
2026-08-10 11:58:33 -0400

    Trabalho 2

--- run.sh
+++ run.sh
@@ -9,7 +9,7 @@ CFLAGS="-std=c11 -O2 -g3 -Wall -Wextra --target=riscv32-unknown-elf -fuse-ld=lld
 
 # Build the kernel
 $CC $CFLAGS -Wl,-Tkernel.ld -Wl,-Map=kernel.map -o kernel.elf \
-    kernel.c
+    kernel.c common.c
 
 # Start QEMU
 $QEMU -machine virt -bios default -nographic -serial mon:stdio --no-reboot \
