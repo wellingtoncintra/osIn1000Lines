30fc9c3fdc7750ec686caa128310d600a8f97ce8
Wellington Cintra <well.cintra00@gmail.com>
2026-08-25 14:24:30 -0400

    Trabalho 4

--- kernel.c
+++ kernel.c
@@ -37,10 +37,8 @@ void boot(void) {
 }
 
 void kernel_main(void) {
-    printf("\n\nHello %s\n", "World!");
-    printf("1 + 2 = %d, %x\n", 1 + 2, 0x1234abcd);
+    memset(__bss, 0, (size_t) __bss_end - (size_t) __bss);
 
-    for (;;) {
-        __asm__ __volatile__("wfi");
-    }
+    PANIC("booted!");
+    printf("unreachable here!\n");
 }
